c166796808ab74b130d53ac314038a2caa51e50f
GerbyPutri <imelkosong212@gmail.com>
2025-05-07 15:01:13 +0700

    Add files via upload
    Praktikum1 Alpro

---
+++ 24221006_Gerby/src/pkg24221006_gerby/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg24221006_gerby;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int nim = 24221006;
+       String nama = "gerby";
+       String alamat = "Ganjuran Kab Bantul";
+       String TTL = "Tegal, 2 November 2004";
+               
+        System.out.println("biodata");
+        
+        System.out.println("NIM = "+nim);
+        System.out.println("Nama = "+nama);
+        System.out.println("Alamat = "+alamat);
+        System.out.println("Ttl = "+TTL);
+        
+  
+    }
+    
+}
